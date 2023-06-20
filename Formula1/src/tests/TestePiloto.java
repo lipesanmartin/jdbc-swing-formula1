c1e5787b3995db7f51beca2265544e429d6ae9f4
Felipe Sanmartin <lipesanmartin@hotmail.com>
2023-06-20 04:04:11 -0300

    testes comentados

--- Formula1/src/tests/TestePiloto.java
+++ Formula1/src/tests/TestePiloto.java
@@ -1,105 +1,105 @@
-package tests;
-
-import java.util.Scanner;
-
-import entities.Piloto;
-
-public class TestePiloto {
-
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		Piloto piloto = new Piloto();
-		int opcao, pilotoId, numCarro, equipeId;
-		String nome, nacionalidade;
-		boolean resultado;
-
-		do {
-			System.out.println("1 - Cadastrar piloto");
-			System.out.println("2 - Consultar piloto");
-			System.out.println("3 - Atualizar numero do carro");
-			System.out.println("4 - Apagar piloto");
-			System.out.println("5 - Encerrar");
-			System.out.print("Entre com uma opção: ");
-			opcao = Integer.parseInt(sc.nextLine());
-			if (opcao < 1 || opcao > 5)
-				System.out.println("Opção incorreta!");
-			else {
-				if (opcao != 5) {
-					try {
-						System.out.print("Entre com o ID do Piloto: ");
-						pilotoId = Integer.parseInt(sc.nextLine());
-						switch (opcao) {
-						case 1:
-							resultado = piloto.consultarPiloto(pilotoId);
-							if (resultado)
-								System.out.println("Piloto já cadastrado!");
-							else {
-								System.out.print("Entre com o nome do piloto: ");
-								nome = sc.nextLine();
-								System.out.print("Entre com a nacionalidade do piloto: ");
-								nacionalidade = sc.nextLine();
-								System.out.print("Entre com o numero do carro do piloto: ");
-								numCarro = Integer.parseInt(sc.nextLine());
-								System.out.print("Entre com a ID da equipe do piloto: ");
-								equipeId = Integer.parseInt(sc.nextLine());
-								piloto.cadastrarPiloto(pilotoId, nome, nacionalidade, numCarro, equipeId);
-							}
-							break;
-						case 2:
-							resultado = piloto.consultarPiloto(pilotoId);
-							if (resultado) {
-								System.out.println("ID: " + piloto.getIdPiloto());
-								System.out.println("Nome: " + piloto.getNome());
-								System.out.println("Nacionalidade: " + piloto.getNacionalidade());
-								System.out.println("Número do carro: " + piloto.getNumCarro());
-								System.out.println("ID da Equipe: " + piloto.getIdEquipe());
-
-							}
-							break;
-						case 3:
-							resultado = piloto.consultarPiloto(pilotoId);
-							if (resultado) {
-								System.out.println("ID: " + piloto.getIdPiloto());
-								System.out.println("Nome: " + piloto.getNome());
-								System.out.println("Número do carro: " + piloto.getNumCarro());
-								System.out.print("Entre com o novo numero do carro: ");
-								numCarro = Integer.parseInt(sc.nextLine());
-								if (!piloto.atualizarNumCarro(pilotoId, numCarro))
-									System.out.println("Erro na atualização do numero do carro!");
-							}
-							break;
-						case 4:
-							resultado = piloto.consultarPiloto(pilotoId);
-							if (resultado) {
-								System.out.println("ID: " + piloto.getIdPiloto());
-								System.out.println("Nome: " + piloto.getNome());
-								System.out.println("Nacionalidade: " + piloto.getNacionalidade());
-								System.out.println("Número do carro: " + piloto.getNumCarro());
-								System.out.print("Tem certeza que quer apagar o piloto (s/n)? ");
-								char decisao = sc.nextLine().charAt(0);
-								if (decisao == 's') {
-									if (!piloto.apagarPiloto(pilotoId)) {
-										System.out.println("Erro, piloto não apagado");
-									}
-
-								} else {
-									System.out.println("Operação cancelada!");
-								}
-							}
-							break;
-						}
-					} catch (NumberFormatException erro) {
-						System.out.println("Entre com o formato correto dos dados!");
-					} catch (Exception erro) {
-						System.out.println("Erro não identificado: " + erro.toString());
-					}
-				}
-
-			}
-			System.out.println();
-		} while (opcao != 5);
-		System.out.println("Programa encerrado!");
-		sc.close();
-	}
-}
+//package tests;
+//
+//import java.util.Scanner;
+//
+//import entities.Piloto;
+//
+//public class TestePiloto {
+//
+//	public static void main(String[] args) {
+//
+//		Scanner sc = new Scanner(System.in);
+//		Piloto piloto = new Piloto();
+//		int opcao, pilotoId, numCarro, equipeId;
+//		String nome, nacionalidade;
+//		boolean resultado;
+//
+//		do {
+//			System.out.println("1 - Cadastrar piloto");
+//			System.out.println("2 - Consultar piloto");
+//			System.out.println("3 - Atualizar numero do carro");
+//			System.out.println("4 - Apagar piloto");
+//			System.out.println("5 - Encerrar");
+//			System.out.print("Entre com uma opção: ");
+//			opcao = Integer.parseInt(sc.nextLine());
+//			if (opcao < 1 || opcao > 5)
+//				System.out.println("Opção incorreta!");
+//			else {
+//				if (opcao != 5) {
+//					try {
+//						System.out.print("Entre com o ID do Piloto: ");
+//						pilotoId = Integer.parseInt(sc.nextLine());
+//						switch (opcao) {
+//						case 1:
+//							resultado = piloto.consultarPiloto(pilotoId);
+//							if (resultado)
+//								System.out.println("Piloto já cadastrado!");
+//							else {
+//								System.out.print("Entre com o nome do piloto: ");
+//								nome = sc.nextLine();
+//								System.out.print("Entre com a nacionalidade do piloto: ");
+//								nacionalidade = sc.nextLine();
+//								System.out.print("Entre com o numero do carro do piloto: ");
+//								numCarro = Integer.parseInt(sc.nextLine());
+//								System.out.print("Entre com a ID da equipe do piloto: ");
+//								equipeId = Integer.parseInt(sc.nextLine());
+//								piloto.cadastrarPiloto(pilotoId, nome, nacionalidade, numCarro, equipeId);
+//							}
+//							break;
+//						case 2:
+//							resultado = piloto.consultarPiloto(pilotoId);
+//							if (resultado) {
+//								System.out.println("ID: " + piloto.getIdPiloto());
+//								System.out.println("Nome: " + piloto.getNome());
+//								System.out.println("Nacionalidade: " + piloto.getNacionalidade());
+//								System.out.println("Número do carro: " + piloto.getNumCarro());
+//								System.out.println("ID da Equipe: " + piloto.getIdEquipe());
+//
+//							}
+//							break;
+//						case 3:
+//							resultado = piloto.consultarPiloto(pilotoId);
+//							if (resultado) {
+//								System.out.println("ID: " + piloto.getIdPiloto());
+//								System.out.println("Nome: " + piloto.getNome());
+//								System.out.println("Número do carro: " + piloto.getNumCarro());
+//								System.out.print("Entre com o novo numero do carro: ");
+//								numCarro = Integer.parseInt(sc.nextLine());
+//								if (!piloto.atualizarNumCarro(pilotoId, numCarro))
+//									System.out.println("Erro na atualização do numero do carro!");
+//							}
+//							break;
+//						case 4:
+//							resultado = piloto.consultarPiloto(pilotoId);
+//							if (resultado) {
+//								System.out.println("ID: " + piloto.getIdPiloto());
+//								System.out.println("Nome: " + piloto.getNome());
+//								System.out.println("Nacionalidade: " + piloto.getNacionalidade());
+//								System.out.println("Número do carro: " + piloto.getNumCarro());
+//								System.out.print("Tem certeza que quer apagar o piloto (s/n)? ");
+//								char decisao = sc.nextLine().charAt(0);
+//								if (decisao == 's') {
+//									if (!piloto.apagarPiloto(pilotoId)) {
+//										System.out.println("Erro, piloto não apagado");
+//									}
+//
+//								} else {
+//									System.out.println("Operação cancelada!");
+//								}
+//							}
+//							break;
+//						}
+//					} catch (NumberFormatException erro) {
+//						System.out.println("Entre com o formato correto dos dados!");
+//					} catch (Exception erro) {
+//						System.out.println("Erro não identificado: " + erro.toString());
+//					}
+//				}
+//
+//			}
+//			System.out.println();
+//		} while (opcao != 5);
+//		System.out.println("Programa encerrado!");
+//		sc.close();
+//	}
+//}
