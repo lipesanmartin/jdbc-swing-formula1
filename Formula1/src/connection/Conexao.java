ce66b16e5e81f6aed8f4ef3820ddab8ef1b5b80c
Daniel Nogueira <daniel.nogueira.filho@gmail.com>
2023-06-24 14:32:50 -0300

    javadoc application
    Documentação da aplicação (testando)

--- Formula1/src/connection/Conexao.java
+++ Formula1/src/connection/Conexao.java
@@ -3,14 +3,21 @@ package connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+/**
+ * A classe Conexao é responsável por iniciar e encerrar a conexão com o banco de dados.
+ */
 public class Conexao {
+	/**
+	 * Estabelece a conexão com o banco de dados MySQL.
+	 *
+	 * @return A conexão estabelecida com o banco de dados.
+	 */
 	public static Connection conectaBanco() {
 		Connection conexao = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			String url = "jdbc:mysql://localhost/Formula1"; // URL do banco de dados
-			String user = "root"; // nome do usu�rio do banco
+			String user = "root"; // nome do usuário do banco
 			String password = "2205"; // senha do banco (deve ser mudada de acordo com o banco)
 			conexao = DriverManager.getConnection(url, user, password);
 		} catch (ClassNotFoundException erro) {
@@ -22,7 +29,11 @@ public class Conexao {
 		}
 		return conexao;
 	}
-
+	/**
+	 * Fecha a conexão com o banco de dados.
+	 *
+	 * @param conexao A conexão a ser fechada.
+	 */
 	public static void fechaConexao(Connection conexao) {
 		try {
 			conexao.close();
