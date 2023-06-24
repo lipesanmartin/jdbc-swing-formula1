88db8aea1f5a9dc9440c23968b892527ede31ced
Daniel Nogueira <daniel.nogueira.filho@gmail.com>
2023-06-24 15:13:02 -0300

    Documentacao (#2)
    * primeirocommit
    
    adicionando javadoc os autores no programa.java
    
    * javadoc application
    
    Documentação da aplicação (testando)

--- Formula1/src/application/Programa.java
+++ Formula1/src/application/Programa.java
@@ -1,8 +1,17 @@
 package application;
 
 import windows.JanelaPrincipal;
-
+/**
+ * A classe Programa é responsável por iniciar a aplicação e apresentar o menu principal.
+ * @author Felipe Sanmartin, Daniel Nogueira, Francisco Ferreira, Igor Rodrigues
+ *
+ */
 public class Programa {
+	/**
+	 * O método main é o ponto de entrada da aplicação.
+	 * Cria uma instância da classe JanelaPrincipal e chama o método apresentarMenu. 
+	 * 
+	 */
 	public static void main(String[] args) {
 		JanelaPrincipal programa = new JanelaPrincipal();
 		programa.apresentarMenu();
