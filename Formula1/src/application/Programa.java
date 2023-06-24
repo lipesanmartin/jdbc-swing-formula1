bff00b53771539b5a8d0ee1dc72715a393d66782
Daniel Nogueira <daniel.nogueira.filho@gmail.com>
2023-06-24 12:01:12 -0300

    meuprimeirocommit
    adicionando javadoc os autores no programa.java

--- Formula1/src/application/Programa.java
+++ Formula1/src/application/Programa.java
@@ -1,7 +1,13 @@
 package application;
 
 import windows.JanelaPrincipal;
-
+/**
+ * 
+ * @author Felipe Sanmartin, Daniel Nogueira, Francisco Ferreira, Igor Rodrigues
+ * 
+ * 
+ *
+ */
 public class Programa {
 	public static void main(String[] args) {
 		JanelaPrincipal programa = new JanelaPrincipal();
